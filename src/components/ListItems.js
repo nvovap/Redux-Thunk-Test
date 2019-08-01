9b5bc9156e0d97e1d77caeb5a8a2e4fc259b95e7
nvovap <nvovap@gmail.com>
2019-08-01 16:10:27 +0300

    first commit

---
+++ src/components/ListItems.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { itemsFetchData, errorAfterFiveSeconds } from '../redux/actions';
+
+
+const mapStateToProps = (state) => {
+    return {
+        items: state.items,
+        hasErrored: state.itemsHasErrored,
+        isLoading: state.itemsIsLoading
+    };
+};
+
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchData: (url) => dispatch(itemsFetchData(url)),
+        getErrorFive: () => dispatch(errorAfterFiveSeconds())
+    };
+};
+
+class ItemList extends Component {
+
+
+    componentDidMount() {
+            //this.props.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
+       this.props.getErrorFive();
+    }
+
+    render() {
+        if (this.props.hasErrored) {
+            return <p>Sorry! There was an error loading the items</p>;
+        }
+
+        if (this.props.isLoading) {
+            return <p>Loading…</p>;
+        }
+
+        return (
+            <ul>
+                {this.props.items.map((item) => (
+                    <li key={item.id}>
+                        {item.label}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemList);
