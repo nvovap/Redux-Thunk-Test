7cbc2ca2e492abd741ba69274f940e8d76c82e91
nvovap <nvovap@gmail.com>
2019-08-06 12:00:58 +0300

    2_06082019

--- src/components/Item.js
+++ src/components/Item.js
@@ -16,24 +16,39 @@ class Item extends Component {
         }) 
     }
 
+
     handleClick(e){
         this.setState((state) => ({
-                showButtonDelete: !state.showButtonDelete
+                showButtonDelete: true
             })
         )
     }
 
 
-    
+    updateInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    HandleClickButtonDone = (e) => {
+        e.stopPropagation()
+
+        this.setState({
+            showButtonDelete: false,
+        })
+
+        this.props.itemsEditElement(this.props.id, this.state.texEdit)
+        
+    }
     
     render() {
         return <div className='Item' onClick={this.handleClick.bind(this)}>
-        {this.state.showButtonDelete ? <input name="name" placeholder="name" onChange={this.updateInput} value={this.state.texEdit}></input> : <div>{this.props.name}</div>}
+        {this.state.showButtonDelete ? <input name="texEdit" placeholder="label" onChange={this.updateInput} value={this.state.texEdit}></input> : <div>{this.props.name}</div>}
         {this.state.showButtonDelete && <button onClick={(e) => {this.props.clickDelete(this.props.id)}}  className="deleteBtn linkBtn">Delete</button>  }
-        {this.state.showButtonDelete && <button onClick={(e) => {this.props.itemsEditElement(this.props.id, this.state.texEdit)}}  className="doneBtn linkBtn">Done</button>}
+        {this.state.showButtonDelete && <button onClick={this.HandleClickButtonDone.bind(this)}  className="doneBtn linkBtn">Done</button>}
     </div>
     }
 }  
 
 export default Item;
-
